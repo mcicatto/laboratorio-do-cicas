fc6e8360ebb721f1a7564fd46ece4b3878437732
mcicatto <matheus.cicatto@escola.pr.gov.br>
2026-08-27 11:19:15 +0000

    .

--- experimento06.js
+++ experimento06.js
@@ -1,6 +1,20 @@
-const boataoAbrir = document.getElementById("btnAbrirModal");
+const botaoAbrir = document.getElementById("btnAbrirModal");
 const modalContainer = document.getElementById("modalContainer");
 
-boataoAbrir.addEventListener("click", function(){
-    
-})
+botaoAbrir.addEventListener("click", function() {
+    modalContainer.innerHTML = `
+        <div class="modal-fundo">
+            <div class="modal-caixa">
+                <h2>Aviso!</h2>
+                <p>Este modal inteiro foi criado pelo JavaScript.</p>
+                <button id="btnFechaModal">Fechar</button>
+            </div>
+        </div>
+    `;
+
+    const botaoFechar = document.getElementById("btnFechaModal");
+
+    botaoFechar.addEventListener("click", function() {
+        modalContainer.innerHTML = "";
+    });
+});
